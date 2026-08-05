b94aadf30818d53108876e8121b167dd8fa80784
Aditya Dev B <adityadevbpadippura@gmail.com>
2026-08-05 16:09:15 +0530

    Time: 0 ms (100%), Space: 46.3 MB (92.24%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans=new int[nums.length];
+        int i=0;
+        int j=n;
+        for(int k=0;k<nums.length;k++){
+            if(k%2==0){
+                ans[k]=nums[i++];
+            }else{
+                ans[k]=nums[j++];
+            }
+        }
+        return ans;
+    }
+}
